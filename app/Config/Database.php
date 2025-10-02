b6decb85443f211a0e406c20456d9bfe47381e7f
Andres <andresalejandro.bautista@gmail.com>
2025-10-02 08:55:47 -0600

    Se incluyo data base

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'urban_beatf',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
